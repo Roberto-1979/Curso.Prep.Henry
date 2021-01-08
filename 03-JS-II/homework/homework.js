380ae9963c29fc853dfa71512da8afb815c53ef8
Roberto-1979 <paul_richard3@hotmail.com>
2021-01-08 13:23:39 -0300

    actuality

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -14,6 +14,15 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if(idioma === 'aleman') {
+    return 'Guten tag!'
+  }else if(idioma === 'mandarin') {
+    return 'Ni Hao!'
+  }else if(idioma === 'ingles') {
+    return 'Hello!'
+  }else {
+    return 'Hola!'
+  }
 }
 
 function esDiezOCinco(numero) {
